f364c480bfb6b001ab7677499c15dcec0e409f66
Aliza Bataluna <alizabataluna1@gmail.com>
2023-12-15 16:56:27 +0800

    refactor: removed profile from navbar

--- BlogWebApp/frontend/src/components/navbar.jsx
+++ BlogWebApp/frontend/src/components/navbar.jsx
@@ -1,12 +1,11 @@
 const Navbar = () => {
   return (
     <div className="flex flex-row items-center bg-[#000517] text-white font-semibold">
-      <div className="w-2/5 ml-10 mr-16">
+      <div className="w-2/5 ml-10 mr-24">
         <h1>APP NAME</h1>
       </div>
       <div className="flex flex-row space-x-5 h-14 justify-center items-center">
         <a href="/home">Home</a>
-        <a href="/profile">Profile</a>
         <a href="/about">About</a>
       </div>
       <div className="ml-auto mr-10">
