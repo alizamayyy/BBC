59779c31e9b99fe5d13c3b4cea55d72cebfa663c
Aliza Bataluna <alizabataluna1@gmail.com>
2023-12-23 21:35:54 +0800

    class and admin input

--- StudentEnrollmentSystem/frontend/src/dashboard/pageComponents/admins-input.tsx
+++ StudentEnrollmentSystem/frontend/src/dashboard/pageComponents/admins-input.tsx
@@ -4,10 +4,36 @@ import { Label } from "@/components/ui/label";
 import { Input } from "../../components/ui/input";
 
 import { useState } from "react";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
 
 const AdminsInput = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+
+  const createAdmin = async () => {
+    const apiUrl = "http://localhost:5000/admin";
+
+    const requestBody = {
+      name,
+      email,
+    };
+
+    try {
+      const response = await axios.post(apiUrl, requestBody);
+
+      if (response.status === 200 || response.status === 201) {
+        console.log("Admin created successfully");
+        console.log(response.data);
+        setName("");
+        setEmail("");
+      } else {
+        console.error("Failed to create admin");
+      }
+    } catch (error) {
+      console.error("Error creating admin", error);
+    }
+  };
 
   return (
     <div className="flex justify-center h-fit">
@@ -34,6 +60,12 @@ const AdminsInput = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
+        <Button
+          onClick={createAdmin}
+          className="bg-blue-500 text-white rounded-lg px-4 py-2"
+        >
+          Create Admin
+        </Button>
       </div>
     </div>
   );
