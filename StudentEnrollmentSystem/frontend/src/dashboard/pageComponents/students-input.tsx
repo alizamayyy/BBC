e69c2b65b3496734c049fc40e239d5c6a6e918ff
Aliza Bataluna <alizabataluna1@gmail.com>
2023-12-23 14:24:53 +0800

    nwcommit

--- StudentEnrollmentSystem/frontend/src/dashboard/pageComponents/students-input.tsx
+++ StudentEnrollmentSystem/frontend/src/dashboard/pageComponents/students-input.tsx
@@ -1,25 +1,52 @@
-"use client";
-
+import { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "../../components/ui/input";
-
-import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar";
+import axios from "axios";
 
 const StudentsInput = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [date, setDate] = useState<Date | undefined>();
+  const [date, setDate] = useState<string | undefined>();
 
   const handleDateSelect = (selectedDate: Date) => {
-    setDate(selectedDate);
+    const dateString = selectedDate.toLocaleDateString();
+    setDate(dateString);
+  };
+
+  const createStudent = async () => {
+    const apiUrl = "http://localhost:5000/student";
+
+    // Default class_id to 0
+    const requestBody = {
+      name,
+      email,
+      date_of_birth: date,
+      class_id: 0,
+    };
+
+    try {
+      const response = await axios.post(apiUrl, requestBody);
+
+      if (response.status === 200 || response.status === 201) {
+        console.log("Student created successfully");
+
+        setName("");
+        setEmail("");
+        setDate("");
+      } else {
+        console.error("Failed to create student");
+      }
+    } catch (error) {
+      console.error("Error creating student", error);
+    }
   };
 
   return (
     <div className="flex justify-center">
       <div className="flex flex-col gap-4">
         <div className="w-96">
-          <Label htmlFor="name">Name</Label>
+          <Label htmlFor="name">Namee</Label>
           <Input
             type="text"
             id="name"
@@ -44,16 +71,18 @@ const StudentsInput = () => {
           <p className="text-sm font-semibold">Date of Birth</p>
           <Calendar
             mode="single"
-            selected={date}
+            selected={date ? new Date(date) : undefined}
             onSelect={handleDateSelect}
             className="rounded-md ml-16"
           />
-          {date && (
-            <p className="text-sm mt-2">
-              Selected Date: {date.toLocaleDateString()}
-            </p>
-          )}
+          {date && <p className="text-sm mt-2">Selected Date: {date}</p>}
         </div>
+        <button
+          onClick={createStudent}
+          className="bg-blue-500 text-white rounded-lg px-4 py-2"
+        >
+          Create Student
+        </button>
       </div>
     </div>
   );
